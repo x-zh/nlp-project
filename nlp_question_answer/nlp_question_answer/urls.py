ad84c74ee92f633e231d3436a73b174c3e9a8ff4
John Qiao <qiaoshun8888@gmail.com>
2014-10-30 18:31:24 -0400

    change to using python and django

---
+++ nlp_question_answer/nlp_question_answer/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import patterns, include, url
+# from django.contrib import admin
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'nlp_question_answer.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    # url(r'^admin/', include(admin.site.urls)),
+)
