ccb1de5f4a79728c0e92f2f1bc93f9dc26794214
Charlie <xinzhou918@gmail.com>
2014-12-18 18:10:20 -0500

    last min update

--- nlp_question_answer/qa/question_parse.py
+++ nlp_question_answer/qa/question_parse.py
@@ -5,7 +5,7 @@ import nltk
 MAPPING = {
     'who': 'PERSON',
     'what': 'OBJECT',
-    'where': 'LOCATION',
+    'where': 'LOC',
     'when': 'DATETIME',
     'why': 'ABSTRACT',
     'how': 'VBP'
@@ -15,6 +15,9 @@ WORDNET = [
     ['master', 'ms', 'm.s.', 'm.s'],
     ['bachelor', 'bs', 'b.s.', 'b.s'],
     ['doctor', 'phd', 'ph.d'],
+    ['financial aid', 'scholarship'],
+    ['dentistry', 'dental'],
+    ['earn', 'receive']
 ]
 
 IGNORE = [
@@ -83,23 +86,34 @@ class Question():
         elif q[0].lower() == 'how':
             if q[1].lower() in ['many', 'much', 'long']:
                 self.question_type = 'NUM'
+            elif q[1].lower() in ['often', 'frequent']:
+                self.question_type = 'DATETIME'
         elif q[0].lower() in MAPPING:
             self.question_type = MAPPING.get(q[0].lower(), 'UNKNOWN')
         else:
             self.question_type = 'UNKNOWN'
 
+    def ie_preprocess(self, document):
+        sentences = nltk.sent_tokenize(document)
+        sentences = [nltk.word_tokenize(sent) for sent in sentences]
+        sentences = [nltk.pos_tag(sent) for sent in sentences]
+        return sentences
+
+    def apply_wordnet(self):
+        document = self.question.lower()
+        for wordgroup in WORDNET:
+            for word in wordgroup:
+                if (word.lower() in document and word.lower() not in IGNORE):
+                        for word2 in wordgroup:
+                            if word2.lower() != word.lower():
+                                self.kw[word2.lower()] = 1
+                        break
+
     def weight_keywords(self):
         if self.kw is None:
             document = self.question
             kw = {}
-
-            def ie_preprocess(document):
-                sentences = nltk.sent_tokenize(document)
-                sentences = [nltk.word_tokenize(sent) for sent in sentences]
-                sentences = [nltk.pos_tag(sent) for sent in sentences]
-                return sentences
-
-            tagged_sentences = ie_preprocess(document)
+            tagged_sentences = self.ie_preprocess(document)
 
             for k, sent in enumerate(tagged_sentences):
                 sent = nltk.ne_chunk(sent)
@@ -114,10 +128,11 @@ class Question():
                         if i[0] not in IGNORE and i[1] in WEIGHT_MAPPING:
                             kw[i[0]] = WEIGHT_MAPPING.get(i[1], 1)
             self.kw = kw
-        if self.question_type == 'OBJECT':
-            self.kw['which'] = 1
+            if self.question_type == 'OBJECT':
+                self.kw['which'] = 1
         return self.kw
 
     def parse_question(self):
         self.parse_type()
         self.weight_keywords()
+        self.apply_wordnet()
